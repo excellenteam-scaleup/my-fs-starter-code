ae9bb3215e843e130d43fe963d07f55cef41ed2b
excellenteam-scaleup <damian.tissembaum@gmail.com>
2026-06-12 11:47:16 +0300

    adding files for template

---
+++ README.md
@@ -0,0 +1,1 @@
+# my-fs-starter-code
---
+++ vfs.h
@@ -0,0 +1,11 @@
+#ifndef __VFS_H__
+#define __VFS_H__
+
+#include "myfs.h"
+
+
+// Run the user interface of the filesystem
+void run_vfs(MyFs &fs);
+
+
+#endif // __VFS_H__
